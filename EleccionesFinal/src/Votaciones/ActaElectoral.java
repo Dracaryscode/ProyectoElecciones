75dc0296b00b7054fca1cf773055a4d6d4f2f3bc
Leonel <Leonel@DESKTOP-JQNCK2P>
2024-11-13 11:31:24 -0500

    votaciones

--- EleccionesFinal/src/Votaciones/ActaElectoral.java
+++ EleccionesFinal/src/Votaciones/ActaElectoral.java
@@ -6,43 +6,79 @@ package Votaciones;
  * @author Leonel
  */
 import java.util.HashMap;
-
+/*Parte realizada por Leonel Campos*/  
 public class ActaElectoral {
-    private String idMesa;
-    private String fechaHora;
-    private int totalVotantesRegistrados;
-    private int totalVotantesParticiparon;
+    private String numeroMesa;
+    private int totalVotantes;
+    private int votosValidos;
+    private int votosEnBlanco;
+    private int votosNulos;
+    private String observaciones;
     private HashMap<String, Integer> votosPorCandidato;
-
-    public ActaElectoral(String idMesa, String fechaHora, int totalVotantesRegistrados, int totalVotantesParticiparon) {
-        this.idMesa = idMesa;
-        this.fechaHora = fechaHora;
-        this.totalVotantesRegistrados = totalVotantesRegistrados;
-        this.totalVotantesParticiparon = totalVotantesParticiparon;
-        this.votosPorCandidato = new HashMap<>();
+    
+    public String getNumeroMesa() {
+        return numeroMesa;
     }
 
-    public void registrarVoto(String candidato) {
-        if (votosPorCandidato.containsKey(candidato)) {
-            votosPorCandidato.put(candidato, votosPorCandidato.get(candidato) + 1);
-        }
+    public int getTotalVotantes() {
+        return totalVotantes;
+    }
+
+    public int getVotosValidos() {
+        return votosValidos;
+    }
+
+    public int getVotosEnBlanco() {
+        return votosEnBlanco;
+    }
+
+    public int getVotosNulos() {
+        return votosNulos;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
     }
 
     public HashMap<String, Integer> getVotosPorCandidato() {
         return votosPorCandidato;
     }
 
-    public String getIdMesa() { 
-        return idMesa; 
+    public ActaElectoral(String numeroMesa, int totalVotantes, int votosValidos, int votosEnBlanco, int votosNulos, String observaciones) {
+        this.numeroMesa = numeroMesa;
+        this.totalVotantes = totalVotantes;
+        this.votosValidos = votosValidos;
+        this.votosEnBlanco = votosEnBlanco;
+        this.votosNulos = votosNulos;
+        this.observaciones = observaciones;
+        this.votosPorCandidato = new HashMap<>();
     }
-    public String getFechaHora() { 
-        return fechaHora; 
-    }
-        public int getTotalVotantesRegistrados() { 
-            return totalVotantesRegistrados; 
+    
+    public void agregarVoto(String candidato) {
+        if (votosPorCandidato.containsKey(candidato)) {
+            int votosActuales = votosPorCandidato.get(candidato);
+            votosPorCandidato.put(candidato, votosActuales + 1);
+        } else {
+            votosPorCandidato.put(candidato, 1);
         }
-    public int getTotalVotantesParticiparon() { 
-        return totalVotantesParticiparon; 
+    }
+    
+    public String generarResumen() {
+        StringBuilder resumen = new StringBuilder();
+        resumen.append("Acta Electoral - Mesa: ").append(numeroMesa).append("\n");
+        resumen.append("Total de Votantes: ").append(totalVotantes).append("\n");
+        resumen.append("Votos Válidos: ").append(votosValidos).append("\n");
+        resumen.append("Votos en Blanco: ").append(votosEnBlanco).append("\n");
+        resumen.append("Votos Nulos: ").append(votosNulos).append("\n");
+        resumen.append("Observaciones: ").append(observaciones).append("\n");
+        resumen.append("Conteo de Votos por Candidato:\n");
+
+        for (String candidato : votosPorCandidato.keySet()) {
+            resumen.append(" - ").append(candidato).append(": ").append(votosPorCandidato.get(candidato)).append(" votos\n");
+        }
+
+        return resumen.toString();
     }
 }
+
 

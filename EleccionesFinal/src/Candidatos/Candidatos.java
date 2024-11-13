c84cb821fe2f80c78d22301b7517cbf140a4b0e8
Angela <>
2024-11-12 22:53:03 -0500

    Agregé las clase Candidatos para que poner los datos de un candidato como nombre dni, también agregé GestionCandidatos para registrar, modificar y eliminar candidatos. Añadí otro NodoCola  y modifique Cola para agregarle algunas funciones más.

---
+++ EleccionesFinal/src/Candidatos/Candidatos.java
@@ -0,0 +1,56 @@
+package Candidatos;
+
+public class Candidatos {
+    public String nombre;
+    public String apellidos;
+    public String dni;
+    public String partidoPolitico;
+    
+
+    public Candidatos() {
+        this.nombre = "";
+        this.apellidos = "";
+        this.dni = "";
+        this.partidoPolitico = "";
+    }
+
+    public Candidatos(String nombre, String apellidos, String dni, String partidoPolitico) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.partidoPolitico = partidoPolitico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPartidoPolitico() {
+        return partidoPolitico;
+    }
+
+    public void setPartidoPolitico(String partidoPolitico) {
+        this.partidoPolitico = partidoPolitico;
+    }
+    
+}
--- EleccionesFinal/src/TDA/Cola.java
+++ EleccionesFinal/src/TDA/Cola.java
@@ -1,20 +1,23 @@
 package TDA;
 
+import TDA.NodoCola;
+import Candidatos.Candidatos;
+
 /**
  *
  * @author USUARIO
  */
 public class Cola {
-    private Nodo front; // Nodo frontal de la cola
-    private Nodo rear; // Nodo final de la cola
+    private NodoCola front; // Nodo frontal de la cola
+    private NodoCola rear; // Nodo final de la cola
     
     public Cola() {
         front = null; //es como si fuera el L, es decir la cabeza
         rear = null;
     }
     
-    public void insertar(int elemento){
-        Nodo nodo = new Nodo(elemento);     
+    public void insertar(Candidatos candidato){
+        NodoCola nodo = new NodoCola(candidato);     
         if(rear == null) {
             front = nodo; 
             rear = nodo;
@@ -25,15 +28,44 @@ public class Cola {
     }
     
     public void mostrar_elementos() {
-        System.out.println("Los elementos de la cola son:");
-        Nodo ptr = front;
+        System.out.println("Lista de candidatos:");
+        NodoCola ptr = front;
         
         if (front == null) {
-            System.out.println("La cola está vacía");
+            System.out.println("La cola esta vaci�a");
         }        
         while(ptr != null) {
-            System.out.print(ptr.value() + "->");
+            System.out.print(ptr.getCandidato() + "->");
             ptr = ptr.next(); //avanzando
         }  
+    }
+    
+    public void eliminar(String dni){
+        NodoCola ptr  = front;
+        NodoCola anterior = null;
+        while(ptr!=null){
+            if(ptr.getCandidato().getDni().equals(dni)){
+            if (anterior == null)
+                front = ptr.next();            
+            else              
+                anterior.setNext(ptr.next());
+            break;            
+            }
+            anterior = ptr;
+            ptr=ptr.next();
+        }
+        System.out.println("Candidato eliminado");
+        mostrar_elementos();
+    }
+    
+    public NodoCola buscar(String dni){
+        NodoCola ptr = front;
+        while(ptr!= null){
+            if (ptr.getCandidato().getDni().equals(dni)) {
+                return ptr;
+            }
+            ptr = ptr.next();
+        }
+        return null;
     }
 }
